f044b52ca9a59021b20473ea55cc73ee8e2e8da8
Naginipython <naginipython332@gmail.com>
2024-01-14 21:25:11 -0500

    Minor name change

--- include/windowsAPI.h
+++ include/windowsAPI.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <windows.h>
@@ -12,44 +11,6 @@ LRESULT CALLBACK window_callback(HWND win, UINT msg, WPARAM wParam, LPARAM lPara
     
 }
 
-bool create_window(int width, int height, char* title) //{
-    // HINSTANCE instance = GetModuleHandleA(0);
-    // WNDCLASS wc = {};
-    // wc.hInstance = instance;
-    // wc.hIcon = LoadIcon(instance, IDI_APPLICATION);
-    // wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    // wc.lpszClassName = title;
-    // wc.lpfnWndProc = DefWindowProcA; // TODO: fix window proc id
-
-//     if (!RegisterClassA(&wc)) {
-//         return false;
-//     }
-
-//     int dwStyle = WS_OVERLAPPEDWINDOW;
-
-//     window = CreateWindowExA(0,
-//                             title,
-//                             title,
-//                             dwStyle,
-//                             100,
-//                             100,
-//                             width,
-//                             height,
-//                             NULL,
-//                             NULL,
-//                             instance,
-//                             NULL
-//     );
-
-//     if (window == NULL) {
-//         return false;
-//     }
-
-//     ShowWindow(window, SW_SHOW);
-
-//     return true;
-// }
+bool create_window(int width, int height, char* title);
 
 void update_window();
-
-#endif
