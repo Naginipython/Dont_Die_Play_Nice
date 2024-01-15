238ebab1f921483534980617f4e35af28ba1ca82
Naginipython <naginipython332@gmail.com>
2024-01-14 21:13:58 -0500

    Created window.cpp, and moved header functions

--- include/windowsAPI.h
+++ include/windowsAPI.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
@@ -5,51 +7,54 @@
 
 HWND window;
 
-bool win_create_window(int width, int height, char* title) {
-    HINSTANCE instance = GetModuleHandleA(0);
-    WNDCLASS wc = {};
-    wc.hInstance = instance;
-    wc.hIcon = LoadIcon(instance, IDI_APPLICATION);
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.lpszClassName = title;
-    wc.lpfnWndProc = DefWindowProcA; // TODO: fix window proc id
+bool win_create_window(int width, int height, char* title); //{
+//     HINSTANCE instance = GetModuleHandleA(0);
+//     WNDCLASS wc = {};
+//     wc.hInstance = instance;
+//     wc.hIcon = LoadIcon(instance, IDI_APPLICATION);
+//     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+//     wc.lpszClassName = title;
+//     wc.lpfnWndProc = DefWindowProcA; // TODO: fix window proc id
 
-    if (!RegisterClassA(&wc)) {
-        return false;
-    }
+//     if (!RegisterClassA(&wc)) {
+//         return false;
+//     }
 
-    int dwStyle = WS_OVERLAPPEDWINDOW;
+//     int dwStyle = WS_OVERLAPPEDWINDOW;
 
-    window = CreateWindowExA(0,
-                            title,
-                            title,
-                            dwStyle,
-                            100,
-                            100,
-                            width,
-                            height,
-                            NULL,
-                            NULL,
-                            instance,
-                            NULL
-    );
+//     window = CreateWindowExA(0,
+//                             title,
+//                             title,
+//                             dwStyle,
+//                             100,
+//                             100,
+//                             width,
+//                             height,
+//                             NULL,
+//                             NULL,
+//                             instance,
+//                             NULL
+//     );
 
-    if (window == NULL) {
-        return false;
-    }
+//     if (window == NULL) {
+//         return false;
+//     }
 
-    ShowWindow(window, SW_SHOW);
+//     ShowWindow(window, SW_SHOW);
 
-    return true;
-}
+//     return true;
+// }
 
-void win_update_window() {
-    MSG msg;
+void win_update_window(); //{
+//     MSG msg;
 
-    while (PeekMessageA(&msg, window, 0, 0, PM_REMOVE)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-}
+//     while (PeekMessageA(&msg, window, 0, 0, PM_REMOVE)) {
+//         TranslateMessage(&msg);
+//         DispatchMessage(&msg);
+//     }
+// }
+
+#else 
+
 
 #endif
---
+++ src/window.cpp
@@ -0,0 +1,48 @@
+#include "windowsAPI.h"
+
+bool win_create_window(int width, int height, char* title) {
+    HINSTANCE instance = GetModuleHandleA(0);
+    WNDCLASS wc = {};
+    wc.hInstance = instance;
+    wc.hIcon = LoadIcon(instance, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.lpszClassName = title;
+    wc.lpfnWndProc = DefWindowProcA; // TODO: fix window proc id
+
+    if (!RegisterClassA(&wc)) {
+        return false;
+    }
+
+    int dwStyle = WS_OVERLAPPEDWINDOW;
+
+    window = CreateWindowExA(0,
+                            title,
+                            title,
+                            dwStyle,
+                            100,
+                            100,
+                            width,
+                            height,
+                            NULL,
+                            NULL,
+                            instance,
+                            NULL
+    );
+
+    if (window == NULL) {
+        return false;
+    }
+
+    ShowWindow(window, SW_SHOW);
+
+    return true;
+}
+
+void win_update_window() {
+    MSG msg;
+
+    while (PeekMessageA(&msg, window, 0, 0, PM_REMOVE)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+}
