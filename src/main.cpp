1943cb454562bf9150fba9e3bfcf0dcf2387c2ab
GraphiKali <104501184+GraphiKali@users.noreply.github.com>
2024-01-16 10:05:46 -0400

    Updated main.cpp + removed platformAPI.cpp
    renamed gameloopRunning to mainloopRunning

--- src/main.cpp
+++ src/main.cpp
@@ -7,27 +7,19 @@
 
 using namespace std;
 
+bool mainloopRunning = true;
 bool debug = true;
-
-bool gameloopRunning = true;
-bool promptedGLRunning = false;
 
 int main() {
     create_window(400, 250, "DDPN");
 
     // mainloop
-    while (gameloopRunning) {
+    while (mainloopRunning) {
         // SM_TRACE("test");
         // SM_WARN("test");
         // SM_ERROR("test");
         // SM_ASSERT(false, "Assert failed");
         update_window();
-        // if (debug) {
-        //     if (!promptedGLRunning) {
-        //         cout << "Running: Gameloop" << endl;
-        //         promptedGLRunning = true;
-        //     }
-        // }
     }
 
     // kill_window();
